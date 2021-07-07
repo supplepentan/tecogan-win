c1a346af680cdd934f9125b82bb7f1992581791c
supplepentan <pentagramog@gmail.com>
2021-07-07 22:11:33 +0900

    update

--- initial_setup.py
+++ initial_setup.py
@@ -6,3 +6,4 @@ os.system("python -m pip uninstall Keras Keras-Applications Keras-Preprocessing"
 os.system("python -m pip install keras==2.3.1")
 os.system("python -m pip install tensorflow-gpu==1.14.0")
 os.system("python -m pip install requests")
+os.system("python -m pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html")
